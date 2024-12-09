f9c770158d0bc930bfcc36bf2a621d1ad0eb43d6
Johnny Parra <juanparra1996@gmail.com>
2024-12-09 13:42:58 -0800

    26 python

---
+++ 26_Easy_Remove-Duplicates-From-Sorted-Array/Python.py
@@ -0,0 +1,14 @@
+from typing import List
+
+def removeDuplicates(nums: List[int]) -> int:
+  i = 1
+  
+  for j in range(1, len(nums)):
+    if nums[j] != nums[i - 1]:
+      nums[i] = nums[j]
+      i +=1
+  
+  return i
+
+print(removeDuplicates([1,1,2]))
+print(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
